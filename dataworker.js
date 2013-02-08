b6970f3e04a76879c693c57561afbb47292ad080
mike <croteau.mike@gmail.com>
2013-02-08 18:26:16 -0500

    web workers

---
+++ dataworker.js
@@ -0,0 +1,26 @@
+var SUCCESS = 200,
+	LOCAL = 0,
+    DONE = 4;
+
+self.addEventListener('message', function(message) {
+	
+	
+    var http = new XMLHttpRequest();
+    var url = message.data.url;
+	
+    http.open("GET", url, false);
+    http.send();
+	
+	self.postMessage({id : message.data.id, status : 'started'})
+		
+	if(http.readyState === DONE && 
+			( http.status == SUCCESS || http.status == LOCAL )){
+		var responseMessage = {
+			id   : message.data.id,
+			status : 'complete',
+			data : http.responseText
+		}
+		self.postMessage(responseMessage);
+	}
+	
+}, false);
